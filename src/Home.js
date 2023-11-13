1983006364956f0dee80b8bec450d29149b163c3
sidartchy <sidartchy203@gmail.com>
2023-11-13 15:15:30 +0545

    fetch dummy data

--- src/Home.js
+++ src/Home.js
@@ -1,38 +1,38 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import BlogList from './BlogList';
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState(
-      [
-        {
-          title: 'Blog 1', 
-          publication: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, voluptatum! Omnis voluptatum, laborum blanditiis in numquam ullam libero cupiditate, soluta laudantium animi commodi natus illum impedit eos beatae dignissimos ab.', 
-          author: 'Siddharth', 
-          id: 1
-        }, 
-        {
-          title: 'Blog 2', 
-          publication: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, laboriosam tenetur est sapiente ut neque? Magni perspiciatis recusandae fugit ut veniam laboriosam, impedit iure? Et perspiciatis suscipit aliquam aliquid ex omnis delectus doloribus magnam quia. Laborum iure cum placeat ipsum!', 
-          author: 'Daju', 
-          id: 2 
-        }, 
-        { 
-          title: 'Blog 3', 
-          publication: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, molestiae consequuntur error numquam libero corrupti aperiam accusantium ratione. Similique reiciendis vitae dolore, iste eveniet aperiam, voluptatibus repellendus debitis quos ad aliquid, sunt vel error repudiandae.', 
-          author: 'Ludwig Van Beethoven', 
-          id: 3
+    const [blogs, setBlogs] = useState(null); 
+    const [isPending, setIsPending] = useState(true); 
+    const [error, setError] = useState(null) ; 
+
+    useEffect(() => {
+      fetch('http://localhost:8000/blogs')
+      .then(res => {
+        if(!res.ok){
+          throw Error('Error fetching data from the resource'); 
         }
-      ]
-    ); 
+        return res.json(); 
+      })
+      .then(data => {
+        
+        setBlogs(data);
+        setIsPending(false); 
+        setError(null); 
+      })
+      .catch( err =>{
+        setError(err.message); 
+        setIsPending(false); 
+      })
+    });
+    
 
     return (
       <div className="home">
-        {blogs.map((blog) => (
-           <div className="blog-preview" key={blog.id}>
-             <h2>{blog.title}</h2>
-            <p>Written by {blog.author}</p>
-           </div>
-        ) )}
+        <div>{error && <div>{error}</div>}</div>
+        {isPending && <h3>Loading data...</h3>}
+        {blogs &&<BlogList blogs={blogs} title='All Blogs' />}
       </div>
     );
   }
